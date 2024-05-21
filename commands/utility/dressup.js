5b21c2f1cef79949288ff3e997d393b1789d5dd6
adlonymous <adlonymous@gmail.com>
2024-05-21 17:41:37 -0400

    Added basic /dressup reply and embed

--- commands/utility/dressup.js
+++ commands/utility/dressup.js
@@ -1,4 +1,45 @@
-const { SlashCommandBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  AttachmentBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+} = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+const monkeDataPath = path.join(
+  __dirname,
+  "..",
+  "..",
+  "data",
+  "monkeList.json"
+);
+
+let jsonData;
+try {
+  jsonData = JSON.parse(fs.readFileSync(monkeDataPath, "utf-8"));
+  console.log("JSON data loaded successfully");
+} catch (error) {
+  console.error("Error reading monke data:", error);
+  process.exit(1);
+}
+
+function findImageByName(monkename) {
+  if (jsonData.mint) {
+    console.log("Mint object found!", jsonData.mint);
+    if (jsonData.mint.name === monkename) {
+      return jsonData.mint.imageUri;
+    } else {
+      console.log(`Name "${monkename}" does not match "${jsonData.mint.name}"`);
+      return null;
+    }
+  } else {
+    console.log("Mint object not found in JSON data");
+    return null;
+  }
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -23,12 +64,48 @@ module.exports = {
 
   async execute(interaction) {
     const generation = interaction.options.getNumber("generation");
-    const number = interaction.options.getNumber("number");
+    const number = interaction.options.getNumber("monke");
 
-    if ((generation = 2 && number > 5000)) {
+    const background = new ButtonBuilder().setLabel("Background");
+
+    if (generation === 2 && number > 5000) {
       return interaction.reply("You can't have a Monke that big!");
-    } else {
-      await interaction.reply(`Hello Gen${generation} #${number} Monke!`);
+    }
+
+    if (generation === 2) {
+      const inputName = `SMB #${number}`;
+      console.log(inputName);
+      const imageUri = findImageByName(inputName);
+
+      console.log(imageUri);
+      const file = new AttachmentBuilder(
+        "https://utfs.io/f/1571c870-c11d-42d4-a767-bd7599ed8d8e-o6rynl.png"
+      );
+
+      const embed = new EmbedBuilder()
+        .setTitle(`Your Monke`)
+        .setColor("#0099ff")
+        .setDescription(`Gen2 Monke #${number}`)
+        .setThumbnail(
+          "https://utfs.io/f/fe2b27a4-d815-4801-bd46-748166eecb3b-18ddfq.png"
+        )
+        .setImage(
+          "https://utfs.io/f/83aad697-0aa7-456f-8bc2-4e35aa06ab02-2fyg.png"
+        ) // Need to include the particular user's monke pic.
+        .addFields(
+          { name: "Type", value: "xyz", inline: true },
+          { name: "Clothes", value: "xyz", inline: true },
+          { name: "Ears", value: "xyz", inline: true },
+          { name: "Mouth", value: "xyz", inline: true },
+          { name: "Eyes", value: "xyz", inline: true },
+          { name: "Hat", value: "xyz", inline: true }
+        )
+        .setTimestamp();
+      await interaction.reply({
+        content: `Hello Gen${generation} #${number} Monke! Welcome to Monke Dress Up! Choose how you want to dress up your Monke`,
+        embeds: [embed],
+        files: [file],
+      });
     }
   },
 };
